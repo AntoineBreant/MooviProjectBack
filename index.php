23c916db64c4255d2d1a2a5df5fe4dfbf68a8c19
antoine <antoine.breant29@gmail.com>
2019-12-15 17:45:56 +0100

    test api

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php phpinfo(); 
-?>
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/albums/photo.php';
+        break;
+    case '' :
+        require __DIR__ . '/albums/photo.php';
+        break;
+}
